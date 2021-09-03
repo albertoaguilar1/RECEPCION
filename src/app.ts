c895b413f26bfc2071318f1ba6b6a84640cd6ea5
jaguialrh <31484862+albertoaguilar1@users.noreply.github.com>
2021-09-03 17:16:44 -0500

    estructura base

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import path from "path";
+
+import { loadApiEndpoints } from "./controllers/api";
+
+// Create Express server
+const app = express();
+
+// Express configuration
+app.set("port", process.env.PORT || 3000);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+loadApiEndpoints(app);
+
+export default app;
